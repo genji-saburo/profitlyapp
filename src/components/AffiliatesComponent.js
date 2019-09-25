9c9b39fb2cdd51a7ddc1b497b105d6f77d6afd56
Haseeb Saif <prosaifhasi@gmail.com>
2019-09-26 01:19:48 +0500

    affiliate page UI completed

--- src/components/AffiliatesComponent.js
+++ src/components/AffiliatesComponent.js
@@ -8,6 +8,14 @@ import FooterComponent from './FooterComponent';
 import iconLight from "../styles/images/icon-light.png";
 import arrowDown from "../styles/images/arrow-down.png";
 import imgProducts from "../styles/images/img-products.png";
+import imgCommision from "../styles/images/commision.png";
+import imgFreeJoin from "../styles/images/free-join.png";
+import imgQuickPayout from "../styles/images/quick-payout.png";
+import imgUnlimitedEarning from "../styles/images/unlimited-earning.png";
+import imgTracking from "../styles/images/tracking.png";
+import imgLink from "../styles/images/icon-link.png";
+import imgShare from "../styles/images/icon-share.png";
+import imgMoney from "../styles/images/icon-dollar.png";
 
 import '../styles/components/affiliatesPage.css';
 import '../styles/index.css';
@@ -67,9 +75,78 @@ class AffiliatesComponent extends Component {
             <Container>
               <Row>
                 <Col xl="12">
+                  <div className="features-wrp">
+                    <div className="feature-box">
+                      <figure><img className="img-fluid" src={imgCommision} alt="" /></figure>
+                      <div className="text">
+                        25% <br/> Commissions
+                      </div>
+                    </div>
+                    <div className="feature-box">
+                      <figure><img className="img-fluid" src={imgFreeJoin} alt="" /></figure>
+                      <div className="text">
+                      Free to <br/> Join
+                      </div>
+                    </div>
+                    <div className="feature-box">
+                      <figure><img className="img-fluid" src={imgQuickPayout} alt="" /></figure>
+                      <div className="text">
+                      Quick <br/> Payouts
+                      </div>
+                    </div>
+                    <div className="feature-box">
+                      <figure><img className="img-fluid" src={imgUnlimitedEarning} alt="" /></figure>
+                      <div className="text">
+                      Unlimited <br/> Earnings
+                      </div>
+                    </div>
+                    <div className="feature-box">
+                      <figure><img className="img-fluid" src={imgTracking} alt="" /></figure>
+                      <div className="text">
+                      Easy Tracking <br/> & Reports
+                      </div>
+                    </div>
+                  </div>
                   <div className="section-heading mb-4">
                     <h1 className="text-white colfax-bold">It’s as Easy as...</h1>
                     <h2>Getting started is easy. Get your unique referral link, share <br/> that link with your audience, and start earning cash.</h2>
+                  </div>
+                  <div className="links-wrp">
+                    <Row>
+                      <Col xl="4">
+                        <div className="link-box mb-4">
+                          <figure><img className="img-fluid" src={imgLink} alt="" /></figure>
+                          <div className="text">
+                            <h5 className="text-white colfax-bold">
+                              <Link to="/">Get Your Unique Link</Link>
+                            </h5>
+                            <p>Begin by signing-up below to the program in just a few clicks, and you’ll instantly get your unique tracking link. </p>
+                          </div>
+                        </div>
+                      </Col>
+                      <Col xl="4">
+                        <div className="link-box mb-4">
+                          <figure><img className="img-fluid" src={imgShare} alt="" /></figure>
+                          <div className="text">
+                            <h5 className="text-white colfax-bold">
+                              <Link to="/">Share Your Link</Link>
+                            </h5>
+                            <p>Promote your link to your audience and friends through YouTube, Facebook, Instagram, emails, or even flyers and posters.</p>
+                          </div>
+                        </div>
+                      </Col>
+                      <Col xl="4">
+                        <div className="link-box mb-4">
+                          <figure><img className="img-fluid" src={imgMoney} alt="" /></figure>
+                          <div className="text">
+                            <h5 className="text-white colfax-bold">
+                              <Link to="/">Earn 20% Commission</Link>
+                            </h5>
+                            <p>Earn 20% commission for every customer that signs-up for Profitly using your unique referral link.</p>
+                          </div>
+                        </div>
+                      </Col>
+                    </Row>
                   </div>
                   <Row className="justify-content-center">
                     <Col xl="2">
@@ -101,7 +178,16 @@ class AffiliatesComponent extends Component {
             <Container>
               <Row>
                 <Col xl="12">
-                <div className="section-heading mb-4">
+                  <div className="partners-logo text-center">
+                    <a href=""><img src="images/amazon.png" alt="" /></a>
+                    <a href=""><img src="images/aliexpress.png" alt="" /></a>
+                    <a href=""><img src="images/gearbest.png" alt="" /></a>
+                    <a href=""><img src="images/wish.png" alt="" /></a>
+                    <a href=""><img src="images/esty.png" alt="" /></a>
+                    <a href=""><img src="images/ebay.png" alt="" /></a>
+                    <a href=""><img src="images/rakuten.png" alt="" /></a>
+									</div>
+                  <div className="section-heading mb-4">
                     <h1 className="text-white colfax-bold">Unlimited Earnings</h1>
                     <h2>We offer a truly unlimited comission structure. The more <br/> referrals you sign-up, the more money you make.</h2>
                   </div>
